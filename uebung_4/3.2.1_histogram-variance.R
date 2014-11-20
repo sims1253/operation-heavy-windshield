23c50383b710855ca2cb7f59c1a76cb7371b89ba
Lasse <lasse.knudsen@tuhh.de>
2014-11-20 11:07:00 +0100

    Aufgabe 3.2 fertig

---
+++ uebung_4/3.2.1_histogram-variance.R
@@ -0,0 +1,10 @@
+histogram_variance <- function(m,n){
+
+	x = multi_stichprobe(m,n);
+	variance = matrix(, nrow = m, ncol = 1);
+	for(i in 1:m){
+		variance[i,1] = var(x[i,]);
+	}
+	
+	hist(variance);
+}
