8c24f8173bd5b7bad284e9f113dc8b15ded4918f
sims1253 <m0hawk@gmx.de>
2014-12-15 21:27:26 +0100

    Aufgabe 1.3

--- uebung_7/7.1_hypothesentest.R
+++ uebung_7/7.1_hypothesentest.R
@@ -2,13 +2,20 @@ hypothesentest <- function(){
 
 	hist_data = matrix(, nrow=90, ncol=1);
 
-	data <- read.table('/mnt/daten/development/git/operation-heavy-windshield/uebung_7/aufgabe1.txt');
+	data <- read.table('C:/Users/m0hawk/Documents/operation-heavy-windshield/uebung_7/aufgabe1.txt');
 
 	for(i in 1:90){
 		hist_data[i,1] = data[i,1];
 	}
 
-	hist_breaks = c(34.0, 35.1, 35.6, 36.1, 36.6, 37.1, 37.6, 38.0, 39.0);
+	hist_breaks = c(33.0, 35.1, 35.6, 36.1, 36.6, 37.1, 37.6, 38.0, 39.0);
 
 	hist(hist_data,hist_breaks);
+	
+	#par(new=TRUE); # Befehl erlaubt mehrere Graphen übereinander zu legen
+	cat("Press [enter]");
+	readline();
+
+	y = rnorm(90, mean=36.44, sd=0.9);
+	hist(y, hist_breaks);
 }
