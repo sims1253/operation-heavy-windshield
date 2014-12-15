746e975c28eb9e6fc1c989786ba41f9f44ad52bc
Lasse <lasse.knudsen@tuhh.de>
2014-12-15 21:42:37 +0100

    7.2.4

---
+++ uebung_7/7.2.4_prozessfaehigkeitsindex.R
@@ -0,0 +1,15 @@
+index <- function(){
+
+	source("7.2.2_abweichung.R");
+	s_a_tmp = abweichung(temp="s_a");
+	s_a = 6 * s_a_tmp;
+
+	xsmax = 3.6;
+	xsmin = 3.2;
+
+	cp = (xsmax - xsmin)/s_a;
+
+	cat("Prozessfähigkeitsindex:\n");
+	return(cp);
+
+}
